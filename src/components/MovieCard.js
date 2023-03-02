212a0be649a77632762b1a7d75a7545a8baf4fa1
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-03-02 11:01:18 +0100

    Fetch of most popular movies ok

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-export default function MovieCard({ image, title, plot }) {
+export default function MovieCard({
+  image,
+  title,
+  actors,
+  rating,
+  releaseYear,
+}) {
   return (
     <article className="movie-card">
       <img src={image} alt={title} />
       <h3>{title}</h3>
-      <h4>{plot}</h4>
+      <p>Released: {releaseYear}</p>
+      <p>Rating: {rating}</p>
+      <h4>Featuring: {actors}</h4>
       <button>Read more</button>
     </article>
   );
