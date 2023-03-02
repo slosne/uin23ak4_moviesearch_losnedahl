668785bf4521f022c75584982cb0da9ab33779c9
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-03-02 11:01:26 +0100

    Fetch of most popular movies ok

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
 export default function MainContent() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://imdb-api.com/en/API/MostPopularMovies/k_qwuodg9k"
+    );
+    const data = await response.json();
+    setMovies(data.items);
+    console.log(data.items);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <>
       <h3>Result:</h3>
-      <MovieCard />
+      {movies?.map((movie, index) => (
+        <MovieCard
+          key={index}
+          image={movie.image}
+          title={movie.title}
+          actors={movie.crew}
+          rating={movie.imDbRating}
+          releaseYear={movie.year}
+        />
+      ))}
     </>
   );
 }
